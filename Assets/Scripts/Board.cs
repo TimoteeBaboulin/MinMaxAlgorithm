3de4760a3e0686742a3c9c106850418c4df9a563
Timotee BABOULIN <t.baboulin@ensicar.lan>
2022-12-07 08:52:29 +0100

    save

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 
 public class Board : MonoBehaviour{
     public Piece[,] CurrentBoard;
     [NonSerialized] public List<Piece> PiecesEaten;
     public PieceSprites Sprites;
+
+    [SerializeField] private GameObject _tileBase;
 
     private int[,] _baseBoard ={
         {-4, -2, -3, -5, -6, -3, -2, -4 },
@@ -78,27 +79,41 @@ public class Board : MonoBehaviour{
             }
         }
         
+        DrawBoard();
         UpdateBoard();
-    }
-
-    private void Update(){
-        
     }
 
     private void UpdateBoard(){
         foreach (var gameObject in _objects){
             Destroy(gameObject);
         }
-        
-        for (int x = 0; x < CurrentBoard.GetLength(0); x++){
-            for (int y = 0; y < CurrentBoard.GetLength(1); y++){
+
+        float height = CurrentBoard.GetLength(0);
+        float length = CurrentBoard.GetLength(1);
+
+        for (int x = 0; x < height; x++){
+            for (int y = 0; y < length; y++){
                 if (CurrentBoard[x, y] != null){
-                    GameObject newPiece = new GameObject();
-                    newPiece.transform.position = new Vector3(x, y, 0);
+                    GameObject newPiece = new GameObject(CurrentBoard[x,y].GetSprite(this).name);
+                    newPiece.transform.position = new Vector3(y - length / 2, x - (height / 2) + 0.5f, 0);
                     var renderer = newPiece.AddComponent<SpriteRenderer>() as SpriteRenderer;
                     renderer.sprite = CurrentBoard[x, y].GetSprite(this);
                     _objects.Add(newPiece);
                 }
+            }
+        }
+    }
+
+    private void DrawBoard(){
+        int height = CurrentBoard.GetLength(0);
+        int length = CurrentBoard.GetLength(1);
+
+        for (int x = 0; x < height; x++){
+            for (int y = 0; y < length; y++){
+                var newTile = Instantiate(_tileBase, new Vector3(y - (length / 2), x - (height / 2) + 0.5f, 1),
+                    Quaternion.identity);
+                Color color = (x + y) % 2 == 0 ? Color.white : Color.black;
+                newTile.GetComponent<MeshRenderer>().material.color = color;
             }
         }
     }
